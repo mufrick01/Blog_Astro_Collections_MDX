c5a6fc6632b1970cc99ab9f3678f38f8f0988ffd
mufrick01 <mrmufrick@gmail.com>
2024-09-09 01:28:36 -0300

    layouts

---
+++ src/site_config/site-config.ts
@@ -0,0 +1,5 @@
+export const SiteConfig = {
+    title:"Mufrick's Blog",
+    description: 'el blog de Mufrick01',
+    image:'https://elcomercio.pe/resizer/v2/6FUBT6XQXNHHNFOMCHIT7I34NA.jpg?auth=75b9cd3f5f728a5e2d90b6996b551d7e7e346d0a63104e3596036c1f214a1b77&width=1200&height=680&quality=75&smart=true'
+}
